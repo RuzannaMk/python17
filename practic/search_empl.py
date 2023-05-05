e35998e9ab43e72bbfad1d8dcfeb3f4e2dfe07e6
mk <rmkrtchyan87@gmail.com>
2023-05-04 20:24:31 -0700

    class 9

---
+++ practic/search_empl.py
@@ -0,0 +1,62 @@
+import unittest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+
+
+class MyTestCase(unittest.TestCase):
+    def test_search_by_job_title(self):
+        browser = self.browser
+        browser.find_element(By.ID, 'txtUsername').send_keys('admin')
+        browser.find_element(By.ID, 'txtPassword').send_keys('password')
+        browser.find_element(By.ID, 'btnLogin').click()
+
+        self.assertIn('/pim/viewEmployeeList', browser.current_url)
+
+        welcome_message = browser.find_element(By.ID, 'welcome').text
+        self.assertEqual('Welcome Admin', welcome_message)  # add assertion here
+
+        browser.find_element(By.ID, 'empsearch_job_title').click()
+        browser.find_element(By.CSS_SELECTOR, '#empsearch_job_title > option:nth-child(8)').click()
+
+        browser.find_element(By.ID, 'searchBtn').click()
+        wait = WebDriverWait(browser,3)
+        wait.until(expected_conditions.element_located_to_be_selected([By.CSS_SELECTOR, '#empsearch_job_title [value="38"]']))
+
+        all_job_title_values = browser.find_elements(By.CSS_SELECTOR, '#resultTable tr>td:nth-child(5)')
+
+        for job_title in all_job_title_values:
+            self.assertEqual('QA Manager', job_title.text)
+
+        browser.find_element(By.ID, 'welcome').click()
+        wait.until(expected_conditions.visibility_of_all_elements_located((By.LINK_TEXT, 'Logout')))
+        browser.find_element(By.LINK_TEXT, 'Logout').click()
+
+        self.assertIn('/auth/login', browser.current_url)
+
+
+
+
+
+
+
+
+    def setUp(self) -> None:
+        browser = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
+        browser.get('http://hrm-online.portnov.com/')
+        self.browser = browser
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
